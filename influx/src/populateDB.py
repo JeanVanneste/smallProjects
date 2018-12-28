f5666d702b6467734899523e01e8ab734f17ec5b
JeanVanneste <jean.vanneste@student.vinci.be>
2018-12-28 14:04:26 +0100

    Creating the random function

--- influx/src/populateDB.py
+++ influx/src/populateDB.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from influxdb import InfluxDB-Client
+from influxdb import InfluxDBClient
 import random
+import math
+
+def getRandomPulse():
+    pulse = math.ceil(random.gauss(70,5))
+    return pulse
 
 def sendQuery():
+    
+
+if __name__ == "__main__":
+    for i in range(0,10):
+        print(getRandomPulse())
