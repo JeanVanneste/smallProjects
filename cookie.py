4eebcdfb84ac8c0e4b710cb1be9cbe29b3d0d9b3
JeanVanneste <jean.vanneste@student.vinci.be>
2019-06-02 12:01:34 +0200

    refactoring pretty display

--- cookie.py
+++ cookie.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
+def prettyDisplay(total):
+    tot3 = total // 1000000
+    tot2 = (total%1000000) // 1000
+    tot1 = total % 1000
+
+    return (str(int(tot3)) + "'" + str(int(tot2)) + "'" + str(int(tot1)))
+
 print("Cookies/sec ?")
 revenue = float(input())
 
 gain = 30*60*2.5*revenue
-print("Gain = "+str(gain))
+print("Gain = "+prettyDisplay(gain))
 
 total = 20.*(gain/3.0)
-
-tot3 = total // 1000000
-tot2 = (total%1000000) // 1000
-tot1 = total % 1000
-
-print(str(int(tot3)) + " " + str(int(tot2)) + " " + str(int(tot1)))
+print("Minimum reserve : " + prettyDisplay(total))
