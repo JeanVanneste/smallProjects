3891a5650cd918d05ec37e20bf6b49995ccd6b04
JeanVanneste <jean.vanneste@student.vinci.be>
2019-06-02 11:28:30 +0200

    Pretty display

--- cookie.py
+++ cookie.py
@@ -7,4 +7,9 @@ gain = 30*60*2.5*revenue
 print("Gain = "+str(gain))
 
 total = 20.*(gain/3.0)
-print(total)
+
+tot3 = total // 1000000
+tot2 = (total%1000000) // 1000
+tot1 = total % 1000
+
+print(str(int(tot3)) + " " + str(int(tot2)) + " " + str(int(tot1)))
